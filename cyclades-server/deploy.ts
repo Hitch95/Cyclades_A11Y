9a6053c2433f3966e6cbc354f63e06aca5ae7785
Bahloul <hitch95@protonmail.com>
2024-11-13 01:23:04 +0100

    small changes for make works serve method from Deno

--- cyclades-server/deploy.ts
+++ cyclades-server/deploy.ts
@@ -2,4 +2,7 @@ import app from './server.ts';
 
 const PORT = 8000;
 
-Deno.serve({ port: PORT }, app.fetch);
+// Convert Oak application to compatible request handler
+Deno.serve({ port: PORT, hostname: '0.0.0.0' }, async (req) => {
+  return (await app.handle(req)) ?? new Response('Not Found', { status: 404 });
+});
